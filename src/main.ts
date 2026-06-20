4f8b8574664264c242ee2b47e66a1f3d88680074
VitorCollin <vitor.collin@gmail.com>
2026-06-20 01:12:44 -0300

    Instalação do Firebase

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({ projectId: "imuni-2ded8", appId: "1:584015208271:web:71692875aca7bcec376f3a", storageBucket: "imuni-2ded8.firebasestorage.app", apiKey: "AIzaSyBFdrzy9DkXQH8XtSmHumEeMCYw-O2aP94", authDomain: "imuni-2ded8.firebaseapp.com", messagingSenderId: "584015208271", measurementId: "G-5YY2YX8T65", projectNumber: "584015208271", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
